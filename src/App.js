52b12188b1daedcbf7b29bf3c58b5bc2fb870f4b
NyKaren <neydekaren@gmail.com>
2019-09-30 17:00:51 -0300

    Input dinâmico

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './InputCustomizado';
 
 //troquei function por class
 class App extends Component {
@@ -82,18 +83,9 @@ class App extends Component {
               <div className="content" id="content">
                 <div className="pure-form pure-form-aligned">
                   <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post"> {/*Pode ser com onSubmit={this.enviaForm.bind(this)}*/}
-                    <div className="pure-control-group">
-                      <label htmlFor="nome">Nome</label> 
-                      <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}  />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="email">Email</label> 
-                      <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}  />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="senha">Senha</label> 
-                      <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}  />                                      
-                    </div>
+                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"  />
+                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="E-mail"  />                  
+                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"  />                                      
                     <div className="pure-control-group">                                  
                       <label></label> 
                       <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
