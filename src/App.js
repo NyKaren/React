cf68984d7593e283251e979bd53c0ad35bbad61e
NyKaren <neydekaren@gmail.com>
2019-10-08 09:35:13 -0300

    Adicionando arquivo para rodar no visual studio code pelo debug

--- src/App.js
+++ src/App.js
@@ -1,76 +1,29 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './InputCustomizado';
+import { FormularioAutor, TabelaAutores } from './Autor';
 
-//troquei function por class
+
 class App extends Component {
+  
   constructor() {
     super();
-    this.state = {lista : [], nome:'',email:'',senha:''};
-    this.enviaForm = this.enviaForm.bind(this); //Pode ser assim tb
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
+    this.state = {lista : []};
   }
-
-    componentDidMount() {
-        $.ajax({
-          //url:"http://localhost:8080/api/autores", teria que ter jdk7 pra conseguir rodar da minha máquina o .jar
-          url:"https://cdc-react.herokuapp.com/api/autores",
-          dataType: 'json',
-          success:function(resposta){
-            this.setState({lista:resposta});
-          }.bind(this) //pegar o this do react e não do JSON
-        });
-    }
-
-    enviaForm(evento) {
-      evento.preventDefault();
-      $.ajax({
-        url:"https://cdc-react.herokuapp.com/api/autores",
-        contentType: 'application/json',
-        dataType:'json',
-        type:'post',
-        data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
-        success: function(resposta){
-            this.setState({lista:resposta});
-        }.bind(this),
-        error: function(resposta){
-            console.log("erro");
-        }
-      });
-    }
-
-    setNome(evento){
-      this.setState({nome:evento.target.value});
-    }
-    setEmail(evento){
-      this.setState({email:evento.target.value});
-    }
-    setSenha(evento){
-      this.setState({senha:evento.target.value});
-    }
 
   render() {
     return (
       <div id="layout">
-        {/*Menu toggle*/}
-      <a href="#menu" id="menuLink" className="menu-link">
-          <span></span>
-      </a>
+          <a href="#menu" id="menuLink" className="menu-link">
+              <span></span>
+          </a>
 
-      <div id="menu">
-          <div className="pure-menu">
+          <div id="menu">
+            <div className="pure-menu">
               <a className="pure-menu-heading" href="#">Company</a>
-
               <ul className="pure-menu-list">
                   <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
                   <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-
                   <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
               </ul>
           </div>
@@ -81,38 +34,8 @@ class App extends Component {
                 <h1>Cadastro de Autores</h1>
               </div>
               <div className="content" id="content">
-                <div className="pure-form pure-form-aligned">
-                  <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post"> {/*Pode ser com onSubmit={this.enviaForm.bind(this)}*/}
-                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"  />
-                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="E-mail"  />                  
-                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"  />                                      
-                    <div className="pure-control-group">                                  
-                      <label></label> 
-                      <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                    </div>
-                  </form>             
-
-                </div>  
-                <div>            
-                  <table className="pure-table">
-                    <thead>
-                      <tr>
-                        <th>Nome</th>
-                        <th>email</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {this.state.lista.map(function(autor){
-                        return(
-                          <tr>
-                            <td>{autor.nome}</td>                
-                            <td>{autor.email}</td>                
-                          </tr>
-                        )
-                      })}
-                    </tbody>
-                  </table> 
-                </div>             
+                <FormularioAutor/>
+                <TabelaAutores/>
               </div>
             </div>       
     </div>
